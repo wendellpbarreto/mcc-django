fa0fb1bd7869f68995c8805906a411f09116e61a
Wendell P. Barreto <wendellp.barreto@gmail.com>
2014-07-02 18:09:06 -0300

    Change media/static url.

--- mcc/museuvirtual/settings.py
+++ mcc/museuvirtual/settings.py
@@ -92,7 +92,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'museuvirtual', 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://www.natalnet.br/pleiades/mcc/media/'
+MEDIA_URL = ROOT_URL + 'media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -103,7 +103,7 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'museuvirtual', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://www.natalnet.br/pleiades/mcc/static/'
+STATIC_URL = ROOT_URL + 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
